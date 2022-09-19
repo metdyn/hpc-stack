e76a5869a5a3efa6b36b4d7c33ed579c0d4d7697
Yonggang Yu <yonggangyu@ucar.edu>
2022-09-19 17:07:02 +0000

    Add config/config_orcus_intel.sh

---
+++ config/config_orcus_intel.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Compiler/MPI combination
+export HPC_COMPILER="intel/2022.2.0"
+export HPC_MPI="mpich/4.0.2"
+export HPC_PYTHON="python/dummy"
+
+# Build options
+export USE_SUDO=N
+export PKGDIR=pkg
+export LOGDIR=log
+export OVERWRITE=N
+export NTHREADS=12
+export   MAKE_CHECK=N
+export MAKE_VERBOSE=N
+export   MAKE_CLEAN=N
+export DOWNLOAD_ONLY=N
+export STACK_EXIT_ON_FAIL=Y
+export WGET="wget -nv"
+
+
+module purge
+module load intel/2022.2.0 
+module load mpich/4.0.2
+module load cmake/3.17.3
+
+				     
+# Build FMS with AVX2 flags
+export STACK_fms_CFLAGS="-march=core-avx2"
+export STACK_fms_FFLAGS="-march=core-avx2"
+
+
+#
+#
+#
+#exit
+#
+#
+#
+#module unuse /nfs/coe-sw/lmod/lmod/modulefiles/compilers
+#
+##exit
+#
+#
+##module use   /glade/p/ral/jntp/GMTB/tools/compiler_mpi_modules/compilers
+##export MODULEPATH_ROOT=/glade/p/ral/jntp/GMTB/tools/compiler_mpi_modules
+#
+#
+#module use   /users/yonggangyu/tools/hpc-stack/compiler_mpi_modules/compilers
+#export MODULEPATH_ROOT=/users/yonggangyu/tools/hpc-stack/compiler_mpi_modules
+#
+#
+#
+#module load ncarcompilers/0.5.0
+#module load ncarenv/1.3
+#
+## Load these basic modules for Cheyenne
+#
